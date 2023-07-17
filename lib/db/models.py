df385ce30496cf9d6e0d08d388fd711cae0d3f58
Christian Garcia <bones192nd@gmail.com>
2023-07-17 11:38:33 -0400

    Add Table class space holder

--- lib/db/models.py
+++ lib/db/models.py
@@ -5,3 +5,53 @@ from sqlalchemy.ext.declarative import declarative_base
 convention = {"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",}
 metadata = MetaData(naming_convention=convention)
 Base = declarative_base(metadata=metadata)
+
+
+class Owner(Base):
+    """
+    __tablename__ 
+
+    table attributes
+
+    pets = relationship
+
+    def __repr__
+
+    """
+
+    pass
+
+class Pet(Base):
+    """
+    __tablename__
+
+    table attributes 
+    + foriegn ID for owner id
+    + foriegn ID for breed id
+    + foriegn ID for toy id
+
+    def __repr__
+    """
+
+    pass
+
+class Breed(Base):
+    """
+    __tablename__
+
+    table attributes 
+
+    def __repr__
+    """
+    pass
+
+class Toy(Base):
+    """
+    __tablename__
+
+    table attributes 
+    
+    def __repr__
+    """
+    pass
+
