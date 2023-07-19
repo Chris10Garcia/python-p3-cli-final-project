a9f55e6e960ce14ad7920e0f1951670fe5737ad1
Christian Garcia <bones192nd@gmail.com>
2023-07-19 13:06:49 -0400

    update .gitignore file

---
+++ lib/db/migrations/versions/b4a0a4b5c197_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b4a0a4b5c197
+Revises: 
+Create Date: 2023-07-19 12:58:20.145683
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4a0a4b5c197'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
