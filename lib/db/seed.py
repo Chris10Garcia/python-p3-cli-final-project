6c9ae1f1a0a5c890bf871ca81d2aa173b6471252
Christian Garcia <bones192nd@gmail.com>
2023-07-21 17:33:55 -0400

    create more seed code

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -13,7 +13,16 @@ session = Session()
 
 fake = Faker()
 
-# delete records
+dog_temperaments = [
+    "shy", "confident", "aggressive", "friendly", "responsive",
+    "playful", "focused", "easily startled", "timid", "sad",
+    "happy", "social", "anxious", "gentle", "silly", "calm", 
+    "stubborn", "affectionate", "loyal", "lazy", "hyper",  
+]
+
+hair_length = [
+    "short", "very short", "bald", "medium", "long", "extra-long"
+]
 
 def delete_records():
     session.query(Owner).delete()
@@ -21,6 +30,22 @@ def delete_records():
     session.query(Breed).delete()
     session.query(Toy).delete()
     session.commit()
+
+def create_record():
+    dogs = [Dog(
+        name = fake.first_name(),
+        age = random.randint(0,15),
+        checked_in = fake.boolean(),
+        temperament = fake.random_element(dog_temperaments)
+    ) for x in range(30)]
+
+    owners = [Owner(
+        name = fake.name(),
+        phone = fake.phone_number(),
+        email = fake.email(),
+        address = fake.street_address()
+    ) for x in range(80)]
+    
 
 # create records
 
