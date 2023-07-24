715bd015bd27091303ef6a53347527927f3fd950
Christian Garcia <bones192nd@gmail.com>
2023-07-24 14:00:32 -0400

    build CLI commands that prints out all dogs, owners, and toys

--- lib/cli.py
+++ lib/cli.py
@@ -4,7 +4,6 @@ import click
 import helpers
 
 
-
 @click.group()
 def cli():
     """ Welcome to the Flatiron Dog Daycare CLI. 
@@ -20,9 +19,37 @@ def get_all_dogs():
     """Prints out all the dogs in the daycare center"""
     dogs = helpers.all_dogs()
 
+    counter = 1
     for dog in dogs:
         click.echo(dog)
+        if counter % 50 == 0:
+            click.prompt("Press enter to continue", default= " ", show_default=False)
+        counter += 1
 
+@cli.command()
+def get_all_owners():
+    """Prints out all the owners in the daycare center"""
+    owners = helpers.all_owners()
+
+    counter = 1
+    for owner in owners:
+        click.echo(owner)
+        if counter % 20 == 0:
+            click.prompt("Press enter to continue", default= " ", show_default=False)
+        counter += 1
+
+
+@cli.command()
+def get_all_toys():
+    """Prints out all the toys in the daycare center"""
+    toys = helpers.all_toys()
+
+    counter = 1
+    for toy in toys:
+        click.echo(toy)
+        if counter % 20 == 0:
+            click.prompt("Press enter to continue", default= " ", show_default=False)
+        counter += 1
 
 if __name__ == "__main__":
     cli()
