02641d46c62cdb7cb1672553b05c95ca4d57de3c
Christian Garcia <bones192nd@gmail.com>
2023-07-24 13:25:49 -0400

    create basic CLI to test pulling all data

--- lib/cli.py
+++ lib/cli.py
@@ -1,1 +1,28 @@
+#!/usr/bin/env python3
 
+import click
+import helpers
+
+
+
+@click.group()
+def cli():
+    """ Welcome to the Flatiron Dog Daycare CLI. 
+    
+    This app manages all the dogs checked into the daycare, as well as their information,
+    owners, favorite toy, and other details 
+    
+    """
+
+
+@cli.command()
+def get_all_dogs():
+    """Prints out all the dogs in the daycare center"""
+    dogs = helpers.all_dogs()
+
+    for dog in dogs:
+        click.echo(dog)
+
+
+if __name__ == "__main__":
+    cli()
