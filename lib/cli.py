005683fa74be62576a68c1973d7081ad519f836d
Christian Garcia <bones192nd@gmail.com>
2023-07-27 19:37:19 -0400

    regorganize code, some minor refactor

--- lib/cli.py
+++ lib/cli.py
@@ -14,6 +14,10 @@ def cli():
     """
 
 @cli.group()
+def create():
+    """Lists all subcommands for creating entities"""
+
+@cli.group()
 def get():
     """Lists all subcommands for getting various data from the dogday care DB"""
 
@@ -25,9 +29,7 @@ def update():
 def delete():
     """Lists all subcommands for deleting entities within the DB"""
 
-@cli.group()
-def create():
-    """Lists all subcommands for creating entities"""
+
 
 
 ##################################
@@ -36,7 +38,6 @@ def create():
 
 
 ## CREATES NEW DOG
-
 
 
 ## can refactor this????
@@ -93,8 +94,77 @@ def new_owner(name, phone, email, address):
 
 
 ##################################
+# READ COMMANDS                  #
+##################################
+
+
+@get.command()
+@click.option("--name", type=click.STRING, required=True, help="Search by name", prompt=True)
+@click.option("--parameter", type=click.Choice(["dog", "breed", "toy", "owner"]), required=True, prompt=True)
+def search_by_name(name, parameter):
+    """NEED TO UPDATE THIS"""
+    result = helpers.search_record(name, parameter)
+    helpers.print_all(result)
+
+
+@get.command()
+@click.option("--id", type=click.INT, required=True, prompt=True)
+@click.option("--parameter", type=click.Choice(["dog", "breed", "toy", "owner"]), required=True, prompt=True)
+def print_details_for(id, parameter):
+    """NEED TO UPDATE THIS"""
+    record = helpers.return_record(id, parameter)
+    helpers.print_details(record)
+
+#parameter can be only breed, toy, dogs
+@get.command()
+@click.option("--parameter", type=click.Choice(("popular_breed", "favorite_toy", "most_dog")), required=True, prompt=True)
+def most_by(parameter):
+    """NEED TO UPDATE THIS"""
+    if parameter == "popular_breed" or parameter == "favorite_toy":
+        dogs = helpers.get_all("dog")
+        param_formated = parameter[parameter.find("_")+1:]
+        records_dict = helpers.build_count_dict(dogs, param_formated)
+        message = "Here are the most popular breeds" if parameter == "popular_breed" else "Here are the most favorited toys"
+    else:
+        owners = helpers.get_all("owner")
+        records_dict = {owner.name : len(owner.dogs) for owner in owners}
+        message = "Here are the owners with the most dogs"
+
+    sorted_records = sorted(records_dict.items(), key=lambda x: x[1], reverse=True)
+
+    click.echo(message)
+    helpers.print_all(sorted_records)
+
+
+
+@get.command()
+@click.option("--parameter", "-p", type=click.Choice(["dog", "owner", "breed", "toy"]), required=True, help="select the type of record to browse", prompt=True)
+def all_records_by(parameter):
+    """Prints out all records based on input parameter"""
+    records = helpers.get_all(parameter)
+    helpers.print_all(records)
+
+
+
+##################################
 # UPDATE COMMANDS                #
 ##################################
+
+###########################
+# TESTING CODE OUT. TRYING TO REFACTOR UPDATING ATTRIBUTES TO A MORE PROGRAMATTIC WAY
+@update.command()
+@click.option("--id", required=True, type=click.INT, prompt=True)
+@click.option("--parameter", type=click.Choice(("dog", "owner")), prompt=True)
+def test_update(id, parameter):
+    """NEED TO UPDATE THIS"""
+    record = helpers.return_record(id, parameter)
+
+    keys = [key for key in record.__dict__ if not key == "_sa_instance_state" and not key == "id"]
+    # results = [click.prompt(key) for key in keys]
+    attribute = click.prompt("Pick attribute to update", type=click.Choice(keys))
+    click.echo(attribute)
+#######################################
+
 
 ## UPDATES DOG ATTRIBUTES
 @update.command()
@@ -102,7 +172,7 @@ def new_owner(name, phone, email, address):
 @click.option("--attribute", "-attr", required=True, type=click.Choice(["name", "age", "temperament", "checked_in", "breed_id", "toy_id"]), )
 @click.option("--value", "-v", required=True, help="multi value inputs must be covered in quotes", metavar="'VALUE IN QUOTES'")
 def attribute_dog(attribute, id, value):
-    """Change the details of an exisiting owner"""
+    """Change the details of an exisiting dog"""
     dog = helpers.return_record(id, "dog")
 
     ## can refactor this??
@@ -210,7 +280,6 @@ def record_from_db(id, parameter):
         click.echo("Deleting an owner record will also delete the following dog records")
         for dog in record.dogs:
             click.echo(dog)
-    
 
     confirm_delete = click.confirm("Are you sure you want to delete the above record(s)?")
 
@@ -222,53 +291,6 @@ def record_from_db(id, parameter):
 
 
 
-##################################
-# READ COMMANDS                  #
-##################################
-
-
-@get.command()
-@click.option("--name", type=click.STRING, required=True, help="Search by name", prompt=True)
-@click.option("--parameter", type=click.Choice(["dog", "breed", "toy", "owner"]), required=True, prompt=True)
-def search_by_name(name, parameter):
-    result = helpers.search_record(name, parameter)
-    helpers.print_all(result)
-
-
-@get.command()
-@click.option("--id", type=click.INT, required=True, prompt=True)
-@click.option("--parameter", type=click.Choice(["dog", "breed", "toy", "owner"]), required=True, prompt=True)
-def print_details_for(id, parameter):
-    record = helpers.return_record(id, parameter)
-    helpers.print_details(record)
-
-#parameter can be only breed, toy, dogs
-@get.command()
-@click.option("--parameter", type=click.Choice(("popular_breed", "favorite_toy", "most_dog")), required=True, prompt=True)
-def most_by(parameter):
-    if parameter == "popular_breed" or parameter == "favorite_toy":
-        dogs = helpers.get_all("dog")
-        param_formated = parameter[parameter.find("_")+1:]
-        records_dict = helpers.build_count_dict(dogs, param_formated)
-        message = "Here are the most popular breeds" if parameter == "popular_breed" else "Here are the most favorited toys"
-    else:
-        owners = helpers.get_all("owner")
-        records_dict = {owner.name : len(owner.dogs) for owner in owners}
-        message = "Here are the owners with the most dogs"
-
-    sorted_records = sorted(records_dict.items(), key=lambda x: x[1], reverse=True)
-
-    click.echo(message)
-    helpers.print_all(sorted_records)
-
-
-
-@get.command()
-@click.option("--parameter", "-p", type=click.Choice(["dog", "owner", "breed", "toy"]), required=True, help="select the type of record to browse", prompt=True)
-def all_records_by(parameter):
-    """Prints out all records based on input parameter"""
-    records = helpers.get_all(parameter)
-    helpers.print_all(records)
 
 
 if __name__ == "__main__":
